67afc4a36b73ea77e71cba9e66e810171e39c5ac
Peter Amelang <peter@Berlin-3.local>
2016-02-08 12:42:21 +0100

    [ADD] Imprint

--- resources/js/z.app.js
+++ resources/js/z.app.js
@@ -10,6 +10,15 @@
 	function initPage() {
 		$('.container').width($('header').width());
 
+		$('.imprint-toggle').on('click', function() {
+			$('#imprint').toggle();
+
+			setTimeout(function() {
+				$('html, body').animate({
+					scrollTop: $("#imprint").offset().top - 100
+				}, 1000);
+			}, 100);
+		});
 	}
 
 })();
